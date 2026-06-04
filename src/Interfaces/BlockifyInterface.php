3f788004d72e57144bd89381d2e911f00cd8f2d2
texditor <texditor@priveted.com>
2026-06-04 22:03:18 +0400

    Feature/v2 support (#28)
    * feat: add strategy v2 support
    
    * fix(package.json): v2.0.0

---
+++ src/Interfaces/BlockifyInterface.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Texditor\Blockify\Interfaces;
+
+interface BlockifyInterface
+{
+    /**
+     * Get current configuration
+     *
+     * @return \Texditor\Blockify\Interfaces\ConfigInterface
+     */
+    public function config(): ConfigInterface;
+
+    /**
+     * Normalizes input string by removing invisible characters
+     *
+     * @param string $input
+     * @param string $mode 'text' or 'preformatted'
+     * @return string
+     */
+    public function normalizeInput(string $input, string $mode = 'text'): string;
+
+    /**
+     * Escapes Unicode control characters in preformatted text
+     *
+     * @param string $text
+     * @return string
+     */
+    public function escapeUnicodeCharsForPre(string $text): string;
+
+    /**
+     * Removes invisible Unicode control characters
+     *
+     * @param string $input
+     * @return string
+     */
+    public function removeInvisibleChars(string $input): string;
+
+    /**
+     * Set and process input data
+     *
+     * @param array|string $data
+     * @return self
+     * @throws \Texditor\Blockify\Exceptions\InvalidJsonDataException
+     */
+    public function setData($data): self;
+
+    /**
+     * Get processed data
+     *
+     * @return array
+     */
+    public function getData(): array;
+
+    /**
+     * Completely removes control characters and special Unicode formatting marks
+     *
+     * @param string $input
+     * @return string
+     */
+    public function removeControlCharacters(string $input): string;
+
+    /**
+     * Filters and validates item attributes using model-defined rules
+     *
+     * @param array $item
+     * @param \Texditor\Blockify\Interfaces\BlockModelInterface $model
+     * @return array
+     */
+    public function applyAttributeRules(array &$item, BlockModelInterface $model): array;
+
+    /**
+     * Filter of data elements
+     *
+     * @param callable $filter
+     * @return self
+     */
+    public function dataFilter(callable $filter): self;
+
+    /**
+     * Filter data according to validation rules
+     *
+     * @param array $data
+     * @param array $rules
+     * @return array|null
+     */
+    public function filterDataWithRules(array $data, array $rules);
+
+    /**
+     * Block filtering
+     *
+     * @param callable $filter
+     * @return self
+     */
+    public function filter(callable $filter): self;
+
+    /**
+     * Whether the structure contains no errors
+     *
+     * @return bool
+     */
+    public function isValid(): bool;
+
+    /**
+     * Get all validation errors
+     *
+     * @return array
+     */
+    public function getErrors(): array;
+
+    /**
+     * Add an error grouped by error code
+     *
+     * @param string $code
+     * @param array $data
+     * @return void
+     */
+    public function addError(string $code, array $data = []): void;
+}
--- src/Models/CodeBlock.php
+++ src/Models/CodeBlock.php
@@ -6,8 +6,131 @@ use Texditor\Blockify\BlockModel;
 
 class CodeBlock extends BlockModel
 {
+    /** @var string The input field name for the block type identifier */
     protected string $inputName = 'code';
+
+    /** @var string The output HTML tag name for rendering */
     protected string $outputName = 'pre';
+
+    /** @var array List of allowed HTML tags (empty = no restrictions) */
     protected array $allowedTags = [];
+
+    /** @var bool Whether the block content is preformatted text */
     protected bool $isPreformatted = true;
+
+    /**
+     * Default list of supported programming languages and file formats.
+     * Used as fallback when no custom languages are set.
+     *
+     * @var array<int, string>
+     */
+    private $defaultLanguages = [
+        'bash',
+        'c',
+        'clojure',
+        'cmake',
+        'cpp',
+        'csharp',
+        'css',
+        'csv',
+        'dart',
+        'diff',
+        'dockerfile',
+        'elixir',
+        'erlang',
+        'fsharp',
+        'go',
+        'graphql',
+        'groovy',
+        'haskell',
+        'hcl',
+        'html',
+        'ini',
+        'java',
+        'javascript',
+        'json',
+        'julia',
+        'kotlin',
+        'latex',
+        'less',
+        'lua',
+        'makefile',
+        'markdown',
+        'matlab',
+        'nginx',
+        'objectivec',
+        'perl',
+        'php',
+        'plaintext',
+        'powershell',
+        'python',
+        'r',
+        'ruby',
+        'rust',
+        'scala',
+        'scss',
+        'shell',
+        'solidity',
+        'sql',
+        'swift',
+        'svelte',
+        'toml',
+        'typescript',
+        'vim',
+        'vue',
+        'wasm',
+        'xml',
+        'yaml',
+    ];
+
+    /**
+     * Custom list of languages set by the user.
+     * If empty, the default list will be used.
+     *
+     * @var array<int, string>
+     */
+    private $languages = [];
+
+    /**
+     * Initialize the block structure on model load.
+     *
+     * Sets up the required fields: type, data, and optional language selector
+     * with values from the configured languages list.
+     *
+     * @return void
+     */
+    public function onLoad(): void
+    {
+        $this->addBlockAttribute('lang', [
+            'type' => 'string',
+            'text' => true,
+            'values' => $this->getLanguages(),
+            'render' => true
+        ]);
+    }
+
+    /**
+     * Override the default languages list with a custom one.
+     *
+     * @param array<int, string> $languages Custom list of supported languages
+     * @return self Returns the instance for method chaining
+     */
+    public function setLanguages(array $languages): self
+    {
+        $this->languages = $languages;
+
+        return $this;
+    }
+
+    /**
+     * Get the list of supported languages.
+     *
+     * Returns custom languages if set, otherwise falls back to the default list.
+     *
+     * @return array<int, string> List of supported language identifiers
+     */
+    public function getLanguages(): array
+    {
+        return !empty($this->languages) ? $this->languages : $this->defaultLanguages;
+    }
 }
